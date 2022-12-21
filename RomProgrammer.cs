1364c7233d046189e21a2fb988b15936067c8a49
ColtenTrainor <90798178+ColtenTrainor@users.noreply.github.com>
2022-12-21 11:14:37 -0500

    optimized rom table layout

--- RomProgrammer.cs
+++ RomProgrammer.cs
@@ -12,7 +12,7 @@ public class RomProgrammer
      * instr7-0, cycle1-0, <0
      * out:
      * instruction_write
-     * register_write
+     * instruction_register_enable
      * register_select_1
      * register_select_0
      * rrs_write
@@ -23,11 +23,11 @@ public class RomProgrammer
      * in:
      * instr7-0, cycle1-0, !=0
      * out:
+     * register write
      * sli_enable
      * add_enable
      * add_sub
      * add_inc
-     * instruction_register_enable
      * skip-forward-enable
      * jump-enable
      */
@@ -44,52 +44,50 @@ public class RomProgrammer
         // rom 1
         for (int i = 0; i < 2048; i++)
         {
-            var opcode = (byte) (i >> 7);
             var rd = (byte) (i >> 5 & 3);
             var rs = (byte) (i >> 3 & 3);
             var cycle = (byte) (i >> 1 & 3);
-            var lessThanZero = (byte) (i & 1);
             
-            // instruction write
+            // instruction write & instruction register enable
             if (cycle != 0b00) 
-                rom1[i] |= 0b10000000;
-            // register write
-            if (cycle == 0b00 && opcode is not 0b0111 or 0b0001 && !(opcode == 0b1000 && rs is 0b00 or 0b01)) // this line is disgusting
-                rom1[i] |= 0b01000000; 
+                rom1[i] |= 0b11000000;
             // register select & rrs write
             if (cycle == 0b01)
-                rom1[i] |= (byte) (rs << 1);
+                rom1[i] |= (byte) (rs << 9);
             else
-                rom1[i] |= (byte) ((rd << 1) & 0b1);
+                rom1[i] |= (byte) ((rd << 7) | 0b000010000);
         }
         
         // rom 2
         for (int i = 0; i < 2048; i++)
         {
             var opcode = (byte) (i >> 7);
-            var rd = (byte) (i >> 5 & 3);
-            var rs = (byte) (i >> 3 & 3);
-            var cycle = (byte) (i >> 1 & 3);
+            var rs = (byte) (i >> 5 & 3);
+            var cycle = (byte) (i >> 3 & 3);
+            var lessThanZero = (byte) (i >> 1 & 1);
             var notEqualZero = (byte) (i & 1);
             
+            // register write
+            if (cycle == 0b00 && opcode is not 0b0111 or 0b0001 && !(opcode == 0b1000 && rs is 0b00 or 0b01)) // this line is disgusting
+                rom1[i] |= 0b10000000; 
             // sli enable
             if (opcode >> 2 != 0b11) 
-                rom2[i] |= 0b10000000;
+                rom2[i] |= 0b01000000;
             // add enable
             if (opcode is not 0b0111 or 0b0001 && !(opcode == 0b1000 && rs is 0b00 or 0b01)) 
-                rom2[i] |= 0b01000000;
+                rom2[i] |= 0b00100000;
             // add sub
             if (opcode == 0b0001 || (opcode == 0b1000 && rs == 0b00)) 
-                rom2[i] |= 0b00100000;
+                rom2[i] |= 0b00010000;
             // add inc
             if (opcode == 0b1000 && rs is 0b00 or 0b01) 
-                rom2[i] |= 0b00010000;
-            // instruction register enable
-            if (cycle != 0b00) 
                 rom2[i] |= 0b00001000;
             // skip forward enable
-            if (cycle == 0b00 || (cycle == 0b10 && opcode == 0b0100 &&
+            if (cycle == 0b00 || (cycle == 0b10 && opcode == 0b0100 && // skipz & skipnz
                                   ((rs == 0b00 && notEqualZero == 0) || (rs == 0b01 && notEqualZero == 1))))
+                rom2[i] |= 0b00000100;
+            if (cycle == 0b00 || (cycle == 0b10 && opcode == 0b0100 && // skipl & skipge
+                                  ((rs == 0b10 && lessThanZero == 1) || (rs == 0b11 && lessThanZero == 0))))
                 rom2[i] |= 0b00000100;
             // jump enable
             if (!(cycle == 0b10 && opcode == 0b0101)) 
